4128f2db8529251b6246b0469b9df97eb8b76f02
leo.shayne <leo.ezra.shayne@gmail.com>
2026-08-03 20:48:54 +0900

    fix: match Chinese ritual typography to approved design

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "@fontsource/cinzel-decorative/400.css";
 import "@fontsource/im-fell-english/400.css";
 import "@fontsource/im-fell-english/400-italic.css";
+import "@fontsource/ma-shan-zheng/400.css";
 import "@fontsource/source-serif-4/400.css";
 import "@fontsource/source-serif-4/600.css";
 import "@fontsource/source-serif-4/400-italic.css";
